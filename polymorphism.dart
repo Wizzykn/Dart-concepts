bb32613189aeb4a98a8d3dc2dfbed55d0da4d022
igitangazaemmy3@gmail.com <you@example.com>
2025-10-14 10:15:37 +0200

    Polymorphism is an amazing OOP concept!

--- polymorphism.dart
+++ polymorphism.dart
@@ -1,3 +1,22 @@
+class Student{
+  String name;
+  String level;
+  String _dob;
+
+  Student(this.name, this.level, this._dob);
+}
+
+class WMHS_student extends Student{
+  String student_id;
+
+  WMHS_student(this.student_id, super.name, super.level, super._dob);
+  void info(){
+    print("\tA WMHS student info:\nId: $student_id\nName: $name\nLevel: $level\nDOB: $_dob");
+  }
+}
+
 void main() {
-  print("Hello, this is Polyumorphism");
+  // print("Hello, this is Polyumorphism");
+  WMHS_student student1 = WMHS_student("098123poi12", "Uwase Kellia", "Level 5", "7/12/2008");
+  student1.info();
 }
