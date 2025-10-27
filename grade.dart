92045c269b150c64aa9a63f802ea8dab720291a9
igitangazaemmy3@gmail.com <you@example.com>
2025-10-27 16:40:49 +0200

    New basics

---
+++ grade.dart
@@ -0,0 +1,17 @@
+void main(){
+  int mark=90;
+  String name="IGITANGAZA EMMY";
+
+if (mark>=90) {
+  print("YOU HAVE GRADE A , MR.${name}");
+}
+else if(mark>=80){
+  print("YOU HAVE GRADE B , MR.${name}");
+}
+else if(mark>=70){
+print("YOU HAVE GRADE C ,MR.${name}");
+}
+else{
+  print("You have under average ,MR.${name}");
+}
+}
